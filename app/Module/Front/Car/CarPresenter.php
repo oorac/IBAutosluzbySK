531b34f10c4d2ff241eda97faacde384ea819aea
Přemek Doležal <premadolezal@gmail.com>
2022-04-05 12:11:32 +0200

    add Project

--- app/Module/Front/Car/CarPresenter.php
+++ app/Module/Front/Car/CarPresenter.php
@@ -9,12 +9,12 @@ final class CarPresenter extends BasePresenter
 {
     public function actionDefault()
     {
-        $carsWithoutImages = $this->auta->getAllInfoAboutCars($this->getParameter('najezd_od'), $this->getParameter('najezd_do'));
+        $carsWithoutImages = $this->car->getAllInfoAboutCars($this->getParameter('najezd_od'), $this->getParameter('najezd_do'));
         $carsWithImages = [];
         if (!empty($carsWithoutImages))
             foreach ($carsWithoutImages as $car)
             {
-                $car['main_images'] = $this->auta->getMainImagesByCarId($car['id']);
+                $car['main_images'] = $this->car->getMainImagesByCarId($car['id']);
                 $carsWithImages[] = $car;
             }
 
@@ -23,10 +23,10 @@ final class CarPresenter extends BasePresenter
 
     public function actionDetail($carId)
     {
-        $car = $this->auta->getAllInfoAboutCarById($carId);
-        $mainImage = $this->auta->getMainImagesByCarId($carId);
-        $otherImages = $this->auta->getOtherImagesByCarId($carId);
-        $parameters = $this->auta->getAllParametersByCarId($carId);
+        $car = $this->car->getAllInfoAboutCarById($carId);
+        $mainImage = $this->car->getMainImagesByCarId($carId);
+        $otherImages = $this->car->getOtherImagesByCarId($carId);
+        $parameters = $this->car->getAllParametersByCarId($carId);
         $this->template->car = $car;
         $this->template->mainImage = $mainImage;
         $this->template->otherImages = $otherImages;
